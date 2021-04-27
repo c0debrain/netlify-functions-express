25c3f5be9faea649496b8145cf8355d1687f6f88
shahin <codebrain@gmail.com>
2021-04-27 16:21:44 +1000

    vue ssr

--- src/App.js
+++ src/App.js
@@ -38,6 +38,9 @@ class App extends Component {
           <a className='link' href="/.netlify/functions/react-express-ssr">
             Example using React serverside rendering
           </a>
+          <a className='link' href="/.netlify/functions/vue-ssr">
+            Example using Vue serverside rendering
+          </a>
         </div>
       </div>
     )
